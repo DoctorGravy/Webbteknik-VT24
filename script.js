ee19d93d575f9cc3c5538a22f85ca039d0a8702d
id22jnm <id22jnm@ad.umu.se>
2024-02-15 13:21:26 +0100

    addad an new js thingy

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-ImageArray = new Array();
-    ImageArray[0] = '1.png';
-    ImageArray[1] = '2.png';
-    ImageArray[2] = '3.png';
-    ImageArray[3] = '4.png';
+var menuItems = document.querySelectorAll('.kat, .dropknapp');
+
+menuItems.forEach(function(item) {
+    item.addEventListener('mouseover', function() {
+        mouseOver(item);
+    });
+
+    item.addEventListener('mouseout', function() {
+        mouseOut(item);
+    });
+});
 
 
-// Durstenfeld shuffle algorithm
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+function mouseOver(variable) {
+	variable.style.color = "red";
+}
+
+function mouseOut(variable) {
+	variable.style.color = "#E6E6E6";
 }
 
 function getRandom() {
@@ -20,3 +26,22 @@ function getRandom() {
 	document.getElementById("randImage3").innerHTML = ('<img src="' + 'bilder/' + ImageArray[2] + '"style= "margin-left: 1.5vw;height: 6vw;"">')
 	document.getElementById("randImage4").innerHTML = ('<img src="' + 'bilder/' + ImageArray[3] + '"style= "margin-left: 1.5vw;height: 6vw;"">')
 }
+
+// Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+ImageArray = new Array();
+ImageArray[0] = '1.png';
+ImageArray[1] = '2.png';
+ImageArray[2] = '3.png';
+ImageArray[3] = '4.png';
+
+document.getElementsByClassName("kategorilänk").addEventListener("mouseover", mouseOver);
+
+
+
